ccbc981553f22d39a8ae0e9ad44166eb712283c4
SanujTiwari <sanujvirat@gmail.com>
2026-06-14 13:07:06 +0530

    Initial project setup with React, Express and Neon DB

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,16 @@
+const register = async (req, res) => {
+  res.json({
+    message: "Register route working",
+  });
+};
+
+const login = async (req, res) => {
+  res.json({
+    message: "Login route working",
+  });
+};
+
+module.exports = {
+  register,
+  login,
+};
