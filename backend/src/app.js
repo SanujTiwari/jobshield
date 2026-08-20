073bb47afbe3d3efa4c60153f68b89b0369e51cc
SanujTiwari <sanujvirat@gmail.com>
2026-08-20 23:11:21 +0530

    updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,8 @@ const userRoutes = require("./routes/userRoutes");
 const jobRoutes = require("./routes/jobRoutes");
 const scanRoutes = require("./routes/scanRoutes");
 const companyRoutes = require("./routes/companyRoutes");
+const reportRoutes = require("./routes/reportRoutes");
+const adminRoutes = require("./routes/adminRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const resumeRoutes = require("./routes/resumeRoutes");
 
@@ -21,6 +23,8 @@ app.use("/api/users", userRoutes);
 app.use("/api/jobs", jobRoutes);
 app.use("/api/scans", scanRoutes);
 app.use("/api/companies", companyRoutes);
+app.use("/api/reports", reportRoutes);
+app.use("/api/admin", adminRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/resume", resumeRoutes);
 
